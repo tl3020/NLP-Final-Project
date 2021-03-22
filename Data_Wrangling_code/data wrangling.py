8b8627ca19dff6495f1ad7b13d7915a9721bb77b
qrguo-0416 <71095541+qrguo-0416@users.noreply.github.com>
2021-03-22 12:38:05 -0400

    Qianrun Guo's Change
    1) I changed the code to subset twitters that are posted in US and in English.
    2) I don't think S&P500 data is included, so I included daily, weekly, and monthly data

--- Data_Wrangling_code/data wrangling.py
+++ Data_Wrangling_code/data wrangling.py
@@ -12,23 +12,23 @@ import pickle
 path = "C:/Users/guoqi/Desktop/nlp_project/April" # path to data 
 
 
-data = pd.DataFrame()
-for root, dirs, files in os.walk(path):
-    try:
-        for word in files:
-            label = word.split(' ')[0]
-            t_path = root + '/' + word
-            f = pd.read_csv(t_path)
-            d = pd.DataFrame({'user_id':f.user_id, 'text':f.text,
-                             'country':f.country_code, 'language':f.lang,
-                             'date':np.repeat(label,len(f))})
-            data = data.append(d)            
-    except Exception as e:
-        print (e)
+#data = pd.DataFrame()
+#for root, dirs, files in os.walk(path):
+#    try:
+#        for word in files:
+#            label = word.split(' ')[0]
+#            t_path = root + '/' + word
+#            f = pd.read_csv(t_path)
+#            d = pd.DataFrame({'user_id':f.user_id, 'text':f.text,
+#                             'country':f.country_code, 'language':f.lang,
+#                             'date':np.repeat(label,len(f))})
+#            data = data.append(d)            
+#    except Exception as e:
+#        print (e)
 
-pickle.dump(data, open(path+"tweets.pkl", "wb"))
+#pickle.dump(data, open(path+"tweets.pkl", "wb"))
 
 my_data = pickle.load(open(path+"tweets.pkl", "rb"))
         
-
+update_data = my_data[(my_data['language'] == 'en') & (my_data['country'] == 'US')]
 
